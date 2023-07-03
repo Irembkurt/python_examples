ff91c241917a29a2a21af09c27a3d73ae50640ec
Irembkurt <irembeyzakurt01@gmail.com>
2023-07-03 16:00:05 +0300

    first commit

---
+++ strings.py
@@ -0,0 +1,16 @@
+name = 'İrem Beyza'
+surname = 'Kurt'
+age = 20
+
+# print( 'My name is ' + name + ' ' + surname + ' and \n I am ' + str(age) + ' years old')
+
+greeting = 'My name is ' + name + ' ' + surname + ' and \n I am ' + str(age) + ' years old'
+
+length = len(greeting)
+
+# print(greeting)
+# print(greeting[1])
+# print(len(greeting))
+# print(greeting[length - 1])
+
+print(greeting[2:5])
